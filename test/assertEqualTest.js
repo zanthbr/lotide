3b544c519e30c6bac61e7478b3f8ae72b6cdb272
zanthbr <zanthblosmanis@gmail.com>
2020-03-09 16:16:19 +0000

    Refactored head and assert eq test cases into test directory

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual('pog','champ');
+assertEqual(1,1);
+assertEqual(1,89);
+assertEqual('pog', 'pog');
---
+++ test/headTest.js
@@ -0,0 +1,4 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual(head('pog'),'p');
+assertEqual(head('keen'),'k');
